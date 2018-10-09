0403c36573e4e2b82e622f61c13a0530a311eddb
Loizani <loizani.slimani@gmail.com>
2018-10-09 20:47:47 +0200

    Structure de depart pour add data

---
+++ cinquiemeartefactid-loiz-firstspring.loizani/src/main/java/org/cinquiememavengroupid/loizani/dao/IListeCoursesDAO.java
@@ -0,0 +1,9 @@
+package org.cinquiememavengroupid.loizani.dao;
+
+import java.util.List;
+
+import org.cinquiememavengroupid.loizani.bean.Course;
+
+public interface IListeCoursesDAO {
+    List<Course> rechercherCourses();
+}
---
+++ cinquiemeartefactid-loiz-firstspring.loizani/src/main/java/org/cinquiememavengroupid/loizani/monservices/IServiceListeCourses.java
@@ -0,0 +1,9 @@
+package org.cinquiememavengroupid.loizani.monservices;
+
+import java.util.List;
+
+import org.cinquiememavengroupid.loizani.bean.Course;
+
+public interface IServiceListeCourses {
+    List<Course> rechercherCourses();
+}
