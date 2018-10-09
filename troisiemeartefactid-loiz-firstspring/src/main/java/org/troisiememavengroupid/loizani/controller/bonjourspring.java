2d9ccaf8e4318d6044f2dbc3ba9268a1b3140b41
Loizani <loizani.slimani@gmail.com>
2018-10-09 17:11:00 +0200

    3 types d'affichage de données sans sgbd  : affichage donnée par pModel, @RequestMapping, @RequestParam

--- troisiemeartefactid-loiz-firstspring/src/main/java/org/troisiememavengroupid/loizani/controller/bonjourspring.java
+++ troisiemeartefactid-loiz-firstspring/src/main/java/org/troisiememavengroupid/loizani/controller/bonjourspring.java
@@ -1,14 +1,13 @@
-package org.secondmavengroupid.loizani.controller;
+package org.troisiememavengroupid.loizani.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ModelAttribute;
+
 
 @Controller
-
 public class bonjourspring {
     
 	public bonjourspring() {
