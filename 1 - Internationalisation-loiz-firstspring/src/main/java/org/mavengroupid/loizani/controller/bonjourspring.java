eb4fc0427ddecc173bcf41690767e1ce29f4239d
Loizani <loizani.slimani@gmail.com>
2018-10-18 15:41:52 +0200

    Partage du modelmap d'une page a l'autre prossible

--- 1 - Internationalisation-loiz-firstspring/src/main/java/org/mavengroupid/loizani/controller/bonjourspring.java
+++ 1 - Internationalisation-loiz-firstspring/src/main/java/org/mavengroupid/loizani/controller/bonjourspring.java
@@ -3,23 +3,30 @@ package org.mavengroupid.loizani.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class bonjourspring {
-    
+    private ModelMap pModmap ;
 	public bonjourspring() {
 		System.out.println("******* constructeur bonjourspring package org.secondmavengroupid.loizani.controller " );	
 	}
 
 	@RequestMapping (value = "/bonjour", method = RequestMethod.GET)
-    public String affichebonjour(final ModelMap pModel) {       	
-			pModel.addAttribute("personne", "Regis");			
-			System.out.println("******* Attribut associé " );
-			return "vues/bonjour";	
-		
+    public String affichebonjour(final ModelMap argModel) {       	
+		    argModel.addAttribute("personne", "Regis");	
+		    pModmap = argModel ;
+		    String strAttPersonne =  (String) argModel.get("personne") ;
+			System.out.println("******* fonction affichebonjour > Attribut personne vaut : " + strAttPersonne);
+			return "vues/bonjour";		
+}
+	
+	@RequestMapping (value = "/test", method = RequestMethod.GET)
+    public String afficheTest() {       				
+			System.out.println("******* test " );
+		    String strAttPersonne =  (String) pModmap.get("personne") ;
+			System.out.println("******* fonction Test > Attribut personne vaut : " + strAttPersonne);
+			return "vues/bonjour";		
 }
 	
 }
