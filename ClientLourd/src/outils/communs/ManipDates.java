fd466933b51c0f08e0744cd2d1aaf3dce33e54ea
Loizani <loizani.slimani@gmail.com>
2018-11-10 21:59:07 +0100

    Sauvegarde code divers projet "ClientLourd"

---
+++ ClientLourd/src/outils/communs/ManipDates.java
@@ -0,0 +1,32 @@
+package outils.communs;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ManipDates {
+	
+	public static void main(String[] args) {
+	try {
+	    Date myDate = new Date();
+	    System.out.println(myDate);
+
+	    SimpleDateFormat mdyFormat = new SimpleDateFormat("MM/dd/yyyy");
+	    SimpleDateFormat dmyFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+	    // Format the date to Strings
+	    String mdy = mdyFormat.format(myDate);
+	    String dmy = dmyFormat.format(myDate);
+
+	    // Results...
+	    System.out.println(mdy);
+	    System.out.println(dmy);
+	    // Parse the Strings back to dates
+	    // Note, the formats don't "stick" with the Date value
+	    System.out.println(mdyFormat.parse(mdy));
+	    System.out.println(dmyFormat.parse(dmy));
+	} catch (ParseException exp) {
+	    exp.printStackTrace();
+	}
+	}
+}
