6ed878164a20dba136ff8291e2861cf45b17ee73
Loizani <loizani.slimani@gmail.com>
2018-12-01 22:13:04 +0100

    APPLI SAMPLE : SPR MVC + SPR SECURITY + HIBERNATE MYSQL (script sql inclus)

--- SpringMVCHibernateWithSpringSecurityExample/src/main/java/com/websystique/springmvc/model/UserProfileType.java
+++ SpringMVCHibernateWithSpringSecurityExample/src/main/java/com/websystique/springmvc/model/UserProfileType.java
@@ -3,9 +3,9 @@ package com.websystique.springmvc.model;
 import java.io.Serializable;
 
 public enum UserProfileType implements Serializable{
-	USER("USER"),
-	DBA("DBA"),
-	ADMIN("ADMIN");
+	USER("USER"), 
+	             DBA("DBA"),
+	                        ADMIN("ADMIN");
 	
 	String userProfileType;
 	
