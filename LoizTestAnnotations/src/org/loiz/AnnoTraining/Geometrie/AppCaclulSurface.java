21f96a8339b102383f90e08ece678eb736d7a1f1
Loizani <loizani.slimani@gmail.com>
2018-12-15 18:00:24 +0100

    sans annotation, ajout setter pour rayon

--- LoizTestAnnotations/src/org/loiz/AnnoTraining/Geometrie/AppCaclulSurface.java
+++ LoizTestAnnotations/src/org/loiz/AnnoTraining/Geometrie/AppCaclulSurface.java
@@ -1,10 +1,19 @@
 package org.loiz.AnnoTraining.Geometrie;
 
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AppCaclulSurface {
 
 	public static void main(String[] argv) {
 		
+		
+		//ApplicationContext objFactory = new AnnotationConfigApplicationContext();
+		
 		SurfaceCercle objSurCer = new SurfaceCercle(4);
+		//SurfaceCercle objSurCer = objFactory.getBean(SurfaceCercle.class) ; 
+		
+		objSurCer.setRayon(4);
 		
 		System.out.println("la surface du cercle de rayon 4cm est : " + objSurCer.getSurface() );
 		
--- LoizTestAnnotations/src/org/loiz/AnnoTraining/Geometrie/SurfaceCercle.java
+++ LoizTestAnnotations/src/org/loiz/AnnoTraining/Geometrie/SurfaceCercle.java
@@ -5,8 +5,16 @@ public class SurfaceCercle {
 	private  double rayon ;
 	private  double surface ;	
 	
+	public SurfaceCercle() {
+		super();
+	}
+
 	public SurfaceCercle(double rayon) {
 		super();
+		this.rayon = rayon;
+	}
+
+	public void setRayon(double rayon) {
 		this.rayon = rayon;
 	}
 
