bd205a9e2ee016a4c5964cfddaf1bf44dfee6b2f
Loizani <loizani.slimani@gmail.com>
2018-12-16 22:33:06 +0100

    meilleure lecture

--- LoizTestAnnotations/src/org/loiz/AnnoTraining/Geometrie/ObjetGeometrique.java
+++ LoizTestAnnotations/src/org/loiz/AnnoTraining/Geometrie/ObjetGeometrique.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Component;
 public class ObjetGeometrique {	
 	
 	
-	private String privNomFigure;
-
 	@Autowired
 	private InterfaceFigureCirculaire privInterfaceObjFigCirc ;
+	
+	private String privNomFigure;
 	
 	public InterfaceFigureCirculaire getPrivInterfaceObjFigCirc() {
 		return privInterfaceObjFigCirc;
