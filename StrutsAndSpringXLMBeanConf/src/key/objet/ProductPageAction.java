ac09bca2ea36c414d1171fb7c354b02597d3d2a8
Loizani <loizani.slimani@gmail.com>
2019-01-16 21:38:22 +0100

    My Struts and Spring Integration Xml Bean Conf (No annotation)

---
+++ StrutsAndSpringXLMBeanConf/src/key/objet/ProductPageAction.java
@@ -0,0 +1,63 @@
+package key.objet;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.ServletActionContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import key.objet.entities.magasin;
+import key.objet.entities.produit;
+import key.objet.service.InterfaceService;
+import key.objet.service.ServiceImpl;
+
+public class ProductPageAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<magasin> ListeMagasins;
+
+	private List<produit> ListeProduits;
+	
+	private Map<magasin, produit> CatalogMap;
+
+	private InterfaceService attrService;
+
+
+	@Override
+	public String execute() throws Exception {
+//		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(ServletActionContext.getServletContext());
+//		attrService = (ServiceImpl) context.getBean("attrService");
+		ListeMagasins = attrService.getListeMagasins();
+		ListeProduits = attrService.getListeProduits();
+		CatalogMap = attrService.getCatalogMap();
+		 
+		return SUCCESS;
+	}
+	
+	public InterfaceService getAttrService() {
+		return attrService;
+	}
+	public void setAttrService(InterfaceService attrService) {
+		this.attrService = attrService;
+	}
+	
+	public List<magasin> getListeMagasins() {
+		return ListeMagasins;
+	}
+
+	public void setListeMagasins(List<magasin> listeMagasins) {
+		ListeMagasins = listeMagasins;
+	}
+
+	public List<produit> getListeProduits() {
+		return ListeProduits;
+	}
+
+	public void setListeProduits(List<produit> listeProduits) {
+		ListeProduits = listeProduits;
+	}
+}
---
+++ StrutsAndSpringXLMBeanConf/src/key/objet/service/InterfaceService.java
@@ -0,0 +1,36 @@
+package key.objet.service;
+
+import java.util.List;
+import java.util.Map;
+
+import key.objet.dao.InterfacefillDataDao;
+import key.objet.entities.magasin;
+import key.objet.entities.produit;
+
+public interface InterfaceService {
+	
+	
+	public void setIDAO(InterfacefillDataDao iDAO);
+
+	
+	
+	public List<magasin> getListeMagasins() ;
+
+
+
+
+
+	public List<produit> getListeProduits() ;
+
+
+
+
+
+	public  Map<magasin, produit> getCatalogMap() ;
+
+
+
+
+		
+	
+}
