fcee7880070692cdd381af9d392b9caa147c127c
Loizani <loizani.slimani@gmail.com>
2019-01-23 22:16:11 +0100

    Struts et Sqpring IOC avec @Autowired, config par classe Java

---
+++ StrutsAndSpringAutowiredJavaConfig/src/key/objet/dao/InterfacefillDataDao.java
@@ -0,0 +1,31 @@
+package key.objet.dao;
+
+
+import java.util.List;
+import java.util.Map;
+
+import key.objet.entities.magasin;
+import key.objet.entities.produit;
+
+
+public interface InterfacefillDataDao {	
+	
+	
+	public List<magasin> getListeMagasins() ;
+
+
+
+	public List<produit> getListeProduits() ;
+
+
+
+
+	public  Map<magasin, produit> getCatalogMap() ;
+
+
+
+	void initialisation();
+
+
+	
+}
